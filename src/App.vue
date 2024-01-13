edbd086bff0d0b32cd052e89a5c1eb158fd03f4c
Matheo Delaunay <matheo.delaunay@adeo.com>
2024-01-13 16:15:55 +0100

    init Vue project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <login-card></login-card>
+      <component :is="currentComponent === 0 ? SignInCard : LoginCard"></component>
+      <button @click="toggleChange=!toggleChange">{{buttonText}}</button>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import {computed, ref} from "vue";
+import LoginCard from "@/components/LoginCard.vue";
+import SignInCard from "@/components/SignInCard.vue";
+
+const toggleChange = ref(false)
+const buttonText = ref('');
+const currentComponent = computed( ()=> {
+  if (toggleChange.value) {
+    buttonText.value = "sing in";
+    return 0;
+  } else {
+    buttonText.value = "login";
+    return 1
+  }
+})
+</script>

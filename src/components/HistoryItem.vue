d359759217916a27004cb28f74eb15e110dc30ab
naudvard <113362141+naudvard@users.noreply.github.com>
2024-01-21 14:51:02 +0100

    feat(BC-41): add history item (#4)

---
+++ src/components/HistoryItem.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {computed, ref} from "vue";
+
+const status = ref("DONE");
+const isDone = computed(() => {
+  return status.value === "DONE";
+});
+const setStatus = computed(() => ({
+  'pi-check': isDone.value,
+  'pi-spin': !isDone.value,
+  'pi-spinner': !isDone.value,
+}));
+const setStyle = computed(() => ({
+  'color': isDone.value ? 'var(--primary-color)' : 'var(--orange-400)'
+}));
+</script>
+
+<template>
+  <div class="grid p-3">
+    <div class="col-2 flex justify-content-center align-items-center">
+      <i class="pi" :class="setStatus" style="font-size: 2rem" :style="setStyle"></i>
+    </div>
+    <div class="col-10">
+      <div class="grid">
+        <div class="col-12" style="color: white">
+          <div class="pr-3" style="text-overflow: ellipsis; white-space: nowrap; overflow-x: hidden">
+            Ici un long titre peut-être trop long pour tenir sur une ligne on sait pas
+          </div>
+        </div>
+        <div class="col-6" style="color: var(--gray-400)">
+          21/01/2024
+        </div>
+        <div class="col-6" style="color: var(--gray-400)">
+          01:18:00
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,69 +1,33 @@
 <script setup lang="ts">
-import {computed, ref} from "vue";
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
-import Password from 'primevue/password';
-
-
-
-
-const email = ref("");
-const password = ref("");
-const regexEmail = /^[\w][\w0-9_@]{2,14}$/
-const errorEmail = computed( () => {
-  if (regexEmail.test(email.value) && password.value.length > 0)
-    return true
-})
-
+import HistoryItem from "@/components/HistoryItem.vue";
+import ScrollPanel from "primevue/scrollpanel";
 </script>
 
 <template>
-
-  <div class="loginCard">
-        <div class="login">
-            <img class="logo"
-                 src="/favicon.ico" />
-            <div class="title">Login</div>
-            <form>
-              <InputText placeholder="Email" v-model="email"/>
-              <Password :feedback="false" placeholder="Password" v-model="password" toggleMask/>
-              <Button :disabled="!errorEmail" >Submit</Button>
-            </form>
-          </div>
-        </div>
+  <div class="grid" style="margin-bottom: -0.5rem">
+    <div class="col-4 p-0" style="background: #282828;">
+      <ScrollPanel class="h-screen" style="width: 100%;">
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+      </ScrollPanel>
+    </div>
+    <div class="col-8 p-0">
+      a
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
- .loginCard {
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   height: 100vh;
- }
- .title {
-   width: 100%;
-   font-size: 1.8rem;
-   margin-bottom: 10px;
-   text-align: center;
- }
- .login {
-   padding: 15px;
-   width: 300px;
-   box-shadow: 0 0 10px rgba(0,0,0,0.1);
- }
- form > * {
-   margin-bottom: 15px;
- }
-
- button, .logo {
-   display: block;
-   margin-left: auto;
-   margin-right: auto;
- }
- form input {
-   width: 100%;
- }
-
-
 </style>

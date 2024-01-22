fe3ff09babcadf9c6d67b72d38af2d052412ae73
Nathan AUDVARD <nathan.audvard@adeo.com>
2024-01-22 20:56:07 +0100

    feat(BC-48): auth

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import {createApp} from 'vue';
+import { createPinia } from 'pinia'
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/lara-light-green/theme.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 
-createApp(App).use(PrimeVue).mount('#app');
+createApp(App)
+    .use(PrimeVue)
+    .use(createPinia())
+    .mount('#app');
